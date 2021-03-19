afab8512dd2e83ffaf6f1e8a09375bb573921def
潘羿 <33997505+paniy@users.noreply.github.com>
2021-03-19 22:35:33 +0800

    Change Default Dir

--- ssl_update.sh
+++ ssl_update.sh
@@ -3,16 +3,22 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 xray_qr_config_file="/usr/local/vmess_qr.json"
+cert_group="nobody"
+idleleo_xray_dir="/usr/bin/idleleo-xray"
+ssl_chainpath="${idleleo_xray_dir}/data"
 domain=$(grep '\"add\"' $xray_qr_config_file | awk -F '"' '{print $4}')
 
 systemctl stop nginx &> /dev/null
 sleep 1
 "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" &> /dev/null
-"/root/.acme.sh"/acme.sh --installcert -d ${domain} --fullchainpath /data/xray.crt --keypath /data/xray.key --ecc
+"/root/.acme.sh"/acme.sh --installcert -d ${domain} --fullchainpath ${ssl_chainpath}/xray.crt --keypath ${ssl_chainpath}/xray.key --ecc
 sleep 1
-chmod -f a+rw /data/xray.crt
-chmod -f a+rw /data/xray.key
-chown -f nobody:nobody /data/xray.crt
-chown -f nobody:nobody /data/xray.key
+
+if [[ $(grep "nogroup" /etc/group) ]]; then
+    cert_group="nogroup"
+fi
+chmod -f a+rw ${ssl_chainpath}/xray.crt
+chmod -f a+rw ${ssl_chainpath}/xray.key
+chown -R nobody:${cert_group} ${ssl_chainpath}/*
 sleep 1
 systemctl start nginx &> /dev/null
