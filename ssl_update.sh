f1f1dee3dda73b2261a90f74ce738faa940b056b
paniy <louisowen@me.com>
2021-10-11 14:55:57 +0800

    ADD Cert check

--- ssl_update.sh
+++ ssl_update.sh
@@ -2,25 +2,25 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-cert_group="nobody"
+cert_group="nogroup"
 idleleo_dir="/etc/idleleo"
 nginx_systemd_file="/etc/systemd/system/nginx.service"
 ssl_chainpath="${idleleo_dir}/cert"
 xray_qr_config_file="${idleleo_dir}/info/vmess_qr.json"
-domain=$(grep '\"host\"' ${xray_qr_config_file} | awk -F '"' '{print $4}')
+host=$(grep '\"host\"' ${xray_qr_config_file} | awk -F '"' '{print $4}')
 bt_nginx=$(grep '\"bt_nginx\"' ${xray_qr_config_file} | awk -F '"' '{print $4}')
 
-[[ -f ${nginx_systemd_file} ]] &&systemctl stop nginx &> /dev/null
+[[ -f ${nginx_systemd_file} ]] && systemctl stop nginx &> /dev/null
 [[ bt_nginx == "Yes" ]] && /etc/init.d/nginx stop &> /dev/null
 wait
 "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" &> /dev/null
-"/root/.acme.sh"/acme.sh --installcert -d ${domain} --fullchainpath ${ssl_chainpath}/xray.crt --keypath ${ssl_chainpath}/xray.key --ecc
+"/root/.acme.sh"/acme.sh --installcert -d ${host} --fullchainpath ${ssl_chainpath}/xray.crt --keypath ${ssl_chainpath}/xray.key --ecc &> /dev/null
 wait
 
 [[ $(grep "nogroup" /etc/group) ]] && cert_group="nogroup"
 chmod -f a+rw ${ssl_chainpath}/xray.crt
 chmod -f a+rw ${ssl_chainpath}/xray.key
-chown -R nobody:${cert_group} ${ssl_chainpath}/*
+chown -fR nobody:${cert_group} ${ssl_chainpath}/*
 wait
 [[ -f ${nginx_systemd_file} ]] && systemctl start nginx &> /dev/null
 [[ bt_nginx == "Yes" ]] && /etc/init.d/nginx start &> /dev/null
