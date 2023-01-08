758a2af8a223e0ac2a4dfb66b033c519c3855542
paniy <louisowen@me.com>
2023-01-08 08:44:27 +0800

    Welcome back @RPRX

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ OK="${Green}[OK]${Font}"
 Error="${RedW}[错误]${Font}"
 Warning="${RedW}[警告]${Font}"
 
-shell_version="1.9.5.5"
+shell_version="1.9.5.6"
 shell_mode="未安装"
 tls_mode="None"
 ws_grpc_mode="None"
@@ -780,8 +780,8 @@ xray_install() {
 xray_update() {
     [[ ! -d /usr/local/etc/xray ]] && echo -e "${GreenBG} 若更新无效, 建议直接卸载再安装! ${Font}"
     echo -e "${Warning} ${GreenBG} 部分新功能需要重新安装才可生效 ${Font}"
-    ## xray_online_version=$(check_version xray_online_version) 紧急更新
-    xray_online_version=$(check_version xray_online_pre_version)
+    xray_online_version=$(check_version xray_online_version)
+    ## xray_online_version=$(check_version xray_online_pre_version)
     if [[ $(info_extraction xray_version) != ${xray_online_version} ]] && [[ ${xray_version} != ${xray_online_version} ]]; then
         if [[ ${auto_update} != "YES" ]]; then
             echo -e "${Warning} ${GreenBG} 检测到存在最新版 ${Font}"
@@ -2977,8 +2977,8 @@ idleleo_commend() {
                     nginx_need_update="${Green}[最新版]${Font}"
                 fi
                 if [[ -f ${xray_qr_config_file} ]] && [[ -f ${xray_conf} ]] && [[ -f /usr/local/bin/xray ]]; then
-                    ## xray_online_version=$(check_version xray_online_version) 紧急更新
-                    xray_online_version=$(check_version xray_online_pre_version)
+                    xray_online_version=$(check_version xray_online_version)
+                    ## xray_online_version=$(check_version xray_online_pre_version)
                     if [[ $(info_extraction xray_version) == null ]]; then
                         xray_need_update="${Green}[已安装] (版本未知)${Font}"
                     elif [[ ${xray_version} != $(info_extraction xray_version) ]] && [[ $(info_extraction xray_version) != ${xray_online_version} ]]; then
