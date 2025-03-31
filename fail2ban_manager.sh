09606889af8de1df03f07d505a4791128ef4f2e8
yunshu <louisowen@me.com>
2025-03-31 21:31:52 +0800

    Optimize the SNI matching process

--- fail2ban_manager.sh
+++ fail2ban_manager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 定义当前版本号
-mf_SCRIPT_VERSION="1.1.2"
+mf_SCRIPT_VERSION="1.1.4"
 
 mf_main_menu() {
     check_system
@@ -70,7 +70,7 @@ mf_configure_fail2ban() {
     # 启用 nginx-no-host 规则
     if [[ ${reality_add_nginx} == "on" ]] && [[ -z $(grep "filter   = nginx-no-host" /etc/fail2ban/jail.local) ]]; then
         mf_create_nginx_no_host_filter
-        sed -i "\$ a\\\n[nginx-no-host]\nenabled  = true\nfilter   = nginx-no-host\nlogpath  = $nginx_dir/logs/error.log\nbantime  = 604800\nmaxretry = 600" /etc/fail2ban/jail.local
+        sed -i "\$ a\\\n[nginx-no-host]\nenabled  = true\nfilter   = nginx-no-host\nlogpath  = $nginx_dir/logs/sni_abnormal.log\nbantime  = 604800\nmaxretry = 3\nfindtime = 120" /etc/fail2ban/jail.local
     fi
     systemctl daemon-reload
     systemctl restart fail2ban
@@ -80,9 +80,10 @@ mf_configure_fail2ban() {
 mf_create_nginx_no_host_filter() {
     local filter_file="/etc/fail2ban/filter.d/nginx-no-host.conf"
     if [[ ! -f "$filter_file" ]]; then
-        cat >"$filter_file" <<EOF
+        cat >"$filter_file" <<'EOF'
 [Definition]
-failregex = \[error\].*?no host in upstream.*?, client: <HOST>,
+datepattern = ^%%d/%%b/%%Y:%%H:%%M:%%S %%z$
+failregex = ^<HOST> \[.*\] \".*\".*\d+$
 ignoreregex =
 EOF
     fi
