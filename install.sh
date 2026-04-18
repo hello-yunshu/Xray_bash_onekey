1791d07f00791dfaaf866071cd01babf220bbe19
yunshu <louisowen@me.com>
2026-04-19 02:31:19 +0800

    fix: support xray x25519 new output format with Password (PublicKey) field

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ OK="${Green}[OK]${Font}"
 Error="${RedW}[$(gettext "错误")]${Font}"
 Warning="${RedW}[$(gettext "警告")]${Font}"
 
-shell_version="2.8.9"
+shell_version="2.8.10"
 shell_mode="$(gettext "未安装")"
 tls_mode="None"
 ws_grpc_mode="None"
@@ -844,7 +844,7 @@ keys_set() {
             keys=$(${xray_bin_dir}/xray x25519 -i "${privateKey}" | tr '\n' ' ')
             ## 兼容之前的xray版本，一个大版本后删除
             if echo "${keys}" | grep -q "Password (PublicKey): "; then
-                password=$(echo "${keys}" | awk -F"Password (PublicKey): " '{print $2}' | awk '{print $1}')
+                password=$(echo "${keys}" | sed 's/.*Password (PublicKey): //' | awk '{print $1}')
             elif echo "${keys}" | grep -q "Password: "; then
                 password=$(echo "${keys}" | awk -F"Password: " '{print $2}' | awk '{print $1}')
             elif echo "${keys}" | grep -q "PublicKey: "; then
@@ -857,7 +857,7 @@ keys_set() {
             privateKey=$(echo "${keys}" | awk -F"PrivateKey: " '{print $2}' | awk '{print $1}')
             ## 兼容之前的xray版本，一个大版本后删除
             if echo "${keys}" | grep -q "Password (PublicKey): "; then
-                password=$(echo "${keys}" | awk -F"Password (PublicKey): " '{print $2}' | awk '{print $1}')
+                password=$(echo "${keys}" | sed 's/.*Password (PublicKey): //' | awk '{print $1}')
             elif echo "${keys}" | grep -q "Password: "; then
                 password=$(echo "${keys}" | awk -F"Password: " '{print $2}' | awk '{print $1}')
             elif echo "${keys}" | grep -q "PublicKey: "; then
